1b182590a15296519cb9b9e7d2118f3a90e79559
CzinkeM <mczinke-dev@proton.me>
2024-10-17 11:12:39 +0200

    Added twitch urls.

---
+++ composeApp/src/commonMain/kotlin/com/github/czinkem/nessaj_twitch_schedule/data/TwitchUrlProvider.kt
@@ -0,0 +1,7 @@
+package com.github.czinkem.nessaj_twitch_schedule.data
+
+object TwitchUrlProvider {
+    const val SCHEDULE_URL = "https://api.twitch.tv/helix/schedule"
+    const val USERS_URL = "https://api.twitch.tv/helix/users"
+    const val GAMES_URL = "https://api.twitch.tv/helix/games"
+}
