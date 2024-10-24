e65ea95dbe576146436860bd511b638fe04b7115
CzinkeM <mczinke-dev@proton.me>
2024-10-24 14:34:22 +0200

    Fixed intent new task issue.

--- composeApp/src/androidMain/kotlin/com/github/czinkem/nessaj_twitch_schedule/domain/AppProvider.android.kt
+++ composeApp/src/androidMain/kotlin/com/github/czinkem/nessaj_twitch_schedule/domain/AppProvider.android.kt
@@ -3,6 +3,7 @@ package com.github.czinkem.nessaj_twitch_schedule.domain
 import android.content.ActivityNotFoundException
 import android.content.Context
 import android.content.Intent
+import android.content.Intent.FLAG_ACTIVITY_NEW_TASK
 import android.net.Uri
 
 actual class AppProvider(
@@ -13,12 +14,14 @@ actual class AppProvider(
         val twitchWebUri = Uri.parse("https://www.twitch.tv/fyrexxx")
 
         val twitchAppIntent = Intent(Intent.ACTION_VIEW, twitchAppUri)
-        twitchAppIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        twitchAppIntent.addFlags(FLAG_ACTIVITY_NEW_TASK)
 
         try {
             context.startActivity(twitchAppIntent)
         } catch (e: ActivityNotFoundException) {
-            val webIntent = Intent(Intent.ACTION_VIEW, twitchWebUri)
+            val webIntent = Intent(Intent.ACTION_VIEW, twitchWebUri).apply {
+                addFlags(FLAG_ACTIVITY_NEW_TASK)
+            }
             context.startActivity(webIntent)
         }
     }
